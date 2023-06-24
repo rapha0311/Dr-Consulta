8622974ed383f36b7a0ca6da06f5e37fd17094e7
rapha0311 <101482337+rapha0311@users.noreply.github.com>
2023-06-23 23:26:15 -0300

    Add files via upload

--- src/js/botao.js
+++ src/js/botao.js
@@ -1,9 +1,19 @@
-const botao = document.querySelector(".btn_fixed");
+const scrollAnima = document.querySelector('[data-anima="scroll"]');
+const metadeWindow = window.innerHeight * 0;
 
-window.addEventListener("scroll", function (event) {
-    if (this.window.scrollY == 0) {
-        botao.classList.remove("visible");
-    } else {
-        botao.classList.add("visible");
-    }
-});
+console.log(metadeWindow)
+
+
+function animarScroll() {
+    const topoItem = scrollAnima.getBoundingClientRect().top;
+    const itemVisivel = topoItem - metadeWindow < 0;
+
+    if(itemVisivel) {
+        scrollAnima.classList.add('show_btn');
+    }else {
+        scrollAnima.classList.remove('show_btn');
+    };
+    
+}
+
+window.addEventListener('scroll', animarScroll);
